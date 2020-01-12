9d026a2a9a360e888bb635c54483152083ce0485
Dmitry <stoitam@bk.ru>
2020-01-12 13:06:20 +0300

    Верстка

--- source/js/favorites-slider.js
+++ source/js/favorites-slider.js
@@ -30,33 +30,3 @@
 
   window.addEventListener('resize', changeSliderSettings);
 })();
-
-'use strict';
-
-(function () {
-  var windowSize = document.body.clientWidth;
-  var tabletWidth = 768;
-
-  var swiper = new Swiper('.new-features__slider', {
-    // autoHeight: true,
-    slidesPerView: 1,
-    loop: true,
-    // spaceBetween: 0,
-    updateOnWindowResize: true,
-    grabCursor: true,
-    
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
-  
-  function changeSliderSettings() {
-    windowSize = document.body.clientWidth;
-
-    swiper.update();
-  };
-
-  window.addEventListener('resize', changeSliderSettings);
-})();
